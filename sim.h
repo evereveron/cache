c0950d4405a299430c39f91b00f173e1e89ac747
Jasmine <jasmine.feng@rutgers.edu>
2013-12-09 12:56:29 -0500

    Update sim.h

--- sim.h
+++ sim.h
@@ -1,12 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
+#ifndef SIM_H_
+#define SIM_H_
 
 
 typedef struct Block{
   unsigned int tag;
   unsigned int index;
+  
+  //0 is invalid (empty). 
+  //1 is valid.
+  //Blocks initialized at 0.
   unsigned int valid;
+  struct Block *next;
 
 }Block;
 
-int* checkHit(char* assoc, int level);
+/*
+typedef struct Queue{
+	struct Block *block;
+	struct Block *next;
+}
+*/
+
+int assocHit(Block** L, int tag, int index);
+int equals(Block* a, Block* b);
+Block* addToQueue(Block* add, Block* queue);
+printList(Block* queue);
+Block* removeFromQueue(Block* queue);
+
+
+#endif
