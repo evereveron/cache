b509c1a653c2a16e98f9aeaa09714fe1484fef80
Jasmine <jasmine.feng@rutgers.edu>
2013-12-09 21:36:55 -0500

    Update testdrive.c

--- testdrive.c
+++ testdrive.c
@@ -2,8 +2,15 @@
 #include<stdlib.h>
 #include"fullass.h"
 #include"sim.h"
+#include"datastructs.h"
+
+
+
 
 main(){
+	int Lsize = 3;
+	Block* queue = (Block*)malloc(sizeof(Block));
+	
 	Block **L;
 		L = malloc(3* sizeof(Block*));
 	Block *L0 = (Block*)malloc(sizeof(Block));
@@ -11,23 +18,29 @@ main(){
 		L0->index = 0;
 		L0->valid = 1;
 	Block *L1 = (Block*)malloc(sizeof(Block));
-		L1->tag = 0x01;
-		L1->index = 0;
+		L1->tag = 0x02;
+		L1->index = 1;
 		L1->valid = 1;
 	Block *L2 = (Block*)malloc(sizeof(Block));
-		L2->tag = 0x01;
+		L2->tag = 0x03;
 		L2->index = 0;
 		L2->valid = 1;
 	
+	
 	L[0] = L0;
+	addToQueue(L0);
 	L[1] = L1;
+	addToQueue(L1);
 	L[2] = L2;
+	addToQueue(L2);
+	
+	printList();
 	
 	//tag = 0x00
 	//index = 2
-	int hit = assocHit(L, 0x00, 2);
+	//int hit = assocHit(L, 0x00, 2);
 	
-	printf("%d\n", hit);
+	//printf("%d\n", hit);
 
 
 
