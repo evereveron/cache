718424defd6fb9707337ea77c6076be552a84328
Jasmine <jasmine.feng@rutgers.edu>
2013-12-10 14:58:40 -0500

    wtf is happening to my linked list

--- testdrive.c
+++ testdrive.c
@@ -8,8 +8,7 @@
 
 
 main(){
-	int Lsize = 3;
-	Block* queue = (Block*)malloc(sizeof(Block));
+	
 	
 	Block **L;
 		L = malloc(3* sizeof(Block*));
@@ -23,18 +22,49 @@ main(){
 		L1->valid = 1;
 	Block *L2 = (Block*)malloc(sizeof(Block));
 		L2->tag = 0x03;
-		L2->index = 0;
+		L2->index = 2;
 		L2->valid = 1;
+	Block *L3 = (Block*)malloc(sizeof(Block));
+		L3->tag = 0x04;
+		L3->index = 3;
+		L3->valid = 1;
+	Block *L4 = (Block*)malloc(sizeof(Block));
+		L4->tag = 0x05;
+		L4->index = 4;
+		L4->valid = 1;
+	Block *L5 = (Block*)malloc(sizeof(Block));
+		L5->tag = 0x06;
+		L5->index = 5;
+		L5->valid = 1;
 	
-	
+	Block *queue = L0;
 	L[0] = L0;
-	addToQueue(L0);
+	addToQueue(queue, L[0]);
 	L[1] = L1;
-	addToQueue(L1);
+	addToQueue(queue, L[1]);
 	L[2] = L2;
-	addToQueue(L2);
+	addToQueue(queue, L[2]);
+	L[3] = L3;
+	addToQueue(queue, L[3]);
+	L[4] = L4;
+		printf("add in 5th element\n");
+	addToQueue(queue, L[4]);
+	L[5] = L5;
+	addToQueue(queue, L[5]);
 	
-	printList();
+	updateQueue(queue, L0);
+
+	/*
+	Block* temp = removeFromQueue(queue);
+	printf("%d\n", temp->tag);
+	temp = removeFromQueue(queue);
+	printf("%d\n", temp->tag);
+	
+	int test = queue->tag;
+	printf("%d\n", test);
+	
+	*/
+	printList(L0);
 	
 	//tag = 0x00
 	//index = 2
